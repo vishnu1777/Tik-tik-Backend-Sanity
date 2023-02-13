ee0407e2382cd3bef592f25214083a0c4c78453b
Vishnu <vappu5429@gmail.com>
2023-02-13 21:59:44 +0530

    final commit

---
+++ schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'comment',
+  type: 'document',
+  title: 'Comment',
+  fields: [
+    {
+      name: 'postedBy',
+      title: 'Posted By',
+      type: 'postedBy',
+    },
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'string',
+    },
+  ],
+}
---
+++ schemas/index.js
@@ -0,0 +1,6 @@
+import comment from './comment'
+import post from './post'
+import user from './user'
+import postedBy from './postedBy'
+
+export const schemaTypes = [user, post, postedBy, comment]
